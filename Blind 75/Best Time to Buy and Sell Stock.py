e0d94957c87cdaeec7ac38a778ba79964e553344
Snighter <60771502+snownighter@users.noreply.github.com>
2024-04-04 13:39:12 +0800

    Create Best Time to Buy and Sell Stock.py

---
+++ Blind 75/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,9 @@
+def maxProfit(prices):
+    if not prices:
+        return 0
+    min_price = float('inf')
+    max_profit = 0
+    for price in prices:
+        min_price = min(min_price, price)
+        max_profit = max(max_profit, price - min_price)
+    return max_profit
