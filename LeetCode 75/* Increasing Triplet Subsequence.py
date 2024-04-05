81869b0c7429c3a11943dfa6821245c8b46fdd0d
Snighter <60771502+snownighter@users.noreply.github.com>
2024-04-05 22:09:59 +0800

    Create * Increasing Triplet Subsequence.py

---
+++ LeetCode 75/* Increasing Triplet Subsequence.py
@@ -0,0 +1,11 @@
+def increasingTriplet(nums):
+    small = float('inf')
+    mid = float('inf')
+    for num in nums:
+        if num <= small:
+            small = num
+        elif num <= mid:
+            mid = num
+        else:
+            return True
+    return False
