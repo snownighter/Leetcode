7301a96210869954d32f202f21b9ed48c0bcea42
Snighter <60771502+snownighter@users.noreply.github.com>
2024-04-25 02:29:16 +0800

    Update (pass) 1995. Count Special Quadruplets.py

--- More Blind 75/(pass) 1995. Count Special Quadruplets.py
+++ More Blind 75/(pass) 1995. Count Special Quadruplets.py
@@ -8,14 +8,7 @@ class Solution:
         for last in range(3, len(nums)): 
             for left in range(0, last-2):
                 for right in range(left+2, last):
-                    i = left + 1
                     outside = nums[last] - (nums[left] + nums[right])
-                    # print(left, i, right)
-                    while i < right:
-                        inside = nums[i]
-                        if inside == outside:
-                            output += 1
-                            # print(nums[left], nums[i], nums[right], nums[last])
-                        i += 1
+                    output += nums[left + 1:right].count(outside)
 
         return output
